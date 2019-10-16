8b4c7c14156766b241e48459cc2cb3ca490ba75a
Devanshi Singh <devanshisingh6b@gmail.com>
2019-10-17 00:49:29 +0530

    Add files via upload
    Program on insertion sort of an array

---
+++ INSERTSO.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<conio.h>
+
+void display(int a[] , int n)
+{
+int i;
+for(i=0; i<n; i++)
+	printf("%d ",a[i]);
+printf("\n");
+}
+
+void insert_sort(int a[], int n)
+{
+int i, j, t;
+for (i = 1 ; i <= n - 1; i++)
+{
+    j = i;
+
+    while ( j > 0 && a[j-1] > a[j])
+    {
+      t = a[j];
+      a[j]   = a[j-1];
+      a[j-1] = t;
+
+      j--;
+    }
+}
+printf("Sorting using Insertion Sort :: ");
+display(a,n);
+}
+
+void main()
+{
+  int n, a[10], i;
+  clrscr();
+  printf("Enter number of elements :: ");
+  scanf("%d", &n);
+  printf("Enter %d numbers\n", n);
+  for (i = 0; i < n; i++)
+	  scanf("%d", &a[i]);
+  insert_sort(a, n);
+  getch();
+}
